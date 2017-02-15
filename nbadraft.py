36799697d0f46dba5a08f9373139460cc14126fa
milankaku <milankaku1@gmail.com>
2017-02-15 16:30:39 -0600

    get nba draft year from the user, some comments for clarity

--- nbadraft.py
+++ nbadraft.py
@@ -2,8 +2,13 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import pandas
 
-draft_year = 2016
+
+draft_year = int(input("Please enter a NBA draft year: "))
+
 url = "http://www.basketball-reference.com/draft/NBA_%d.html" % draft_year
+
+# draft_year = 2016
+# url = "http://www.basketball-reference.com/draft/NBA_.html"
 
 html_from_url = urlopen(url)
 
@@ -13,6 +18,7 @@ table_row = bs.findAll('tr', limit=2)[1].findAll('th')
 
 headers = [th.getText() for th in bs.findAll('tr', limit=2)[1].findAll('th')]
 
+#player data starts after 2nd table record
 player_data_rows = bs.findAll('tr')[2:]
 
 player_data = [[td.getText() for td in player_data_rows[i].findAll('td')]
@@ -23,6 +29,7 @@ headers.remove('Pk')
 
 data_frame = pandas.DataFrame( player_data, columns=headers )
 
+#remove any rows that have null data
 data_frame = data_frame[data_frame.Player.notnull()]
 
 # Make header names more readable/clear
@@ -33,9 +40,10 @@ data_frame.columns.values[13:18] = [data_frame.columns.values[13:18][col] + " pe
 
 data_frame = data_frame.convert_objects(convert_numeric=True)
 
-data_frame = data_frame[:].fillna(0) #fill Not A Numbers to 0
-
+#fill Not A Numbers to 0 and change some columns to int
+data_frame = data_frame[:].fillna(0)
 data_frame.loc[:,'Yrs':'AST'] = data_frame.loc[:, 'Yrs':'AST'].astype(int)
+
 data_frame.insert(1, 'Draft Year', draft_year)
 
-print(data_frame)
+print(data_frame.head())
