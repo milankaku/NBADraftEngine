2e4bbfea98438af671fb3e83fc3e3b30e98a7ad7
milankaku <milankaku1@gmail.com>
2017-03-01 12:20:41 -0600

    Add nba draft ipython notebook and start plotting of winshares per 48 by draft year

--- nbadraft.py
+++ nbadraft.py
@@ -30,7 +30,7 @@ for year in range(1996, 2017):
                    for i in range(len(player_data_rows))]
 
     year_data_frame = pandas.DataFrame(player_data, columns=headers)
-    year_data_frame.insert(0, 'Draft Year', year)
+    year_data_frame.insert(0, 'Draft_Year', year)
 
     all_drafts = all_drafts.append(year_data_frame, ignore_index=True)
 
@@ -44,12 +44,22 @@ all_drafts = all_drafts[all_drafts.Player.notnull()]
 all_drafts = all_drafts[:].fillna(0)
 
 # Make header names more readable/clear
-all_drafts.rename(columns={'WS/48': 'WS per 48'}, inplace=True)
+all_drafts.rename(columns={'WS/48': 'WS_per_48'}, inplace=True)
 all_drafts.columns = all_drafts.columns.str.replace('%', '_PERC')
 
-all_drafts.columns.values[13:18] = [all_drafts.columns.values[13:18][col] + " per Game" for col in range(5)]
+all_drafts.columns.values[13:18] = [all_drafts.columns.values[13:18][col] + "_per_Game" for col in range(5)]
 
 all_drafts.loc[:,'Yrs':'AST'] = all_drafts.loc[:,'Yrs':'AST'].astype(int)
 
 all_drafts.to_csv("draft_data_1996_to_2016.csv")
 
+draft_df = pandas.read_csv("draft_data_1996_to_2016.csv", index_col=0)
+
+WS48_yearly_avg = draft_df.groupby('Draft_Year').WS_per_48.mean()
+
+#plot WS per 48 yearly averages
+seaborn.set_style("white")
+ppt.figure(figsize=(13,10))
+
+x_values = draft_df.Draft_Year.unique()
+y_values = WS48_yearly_avg
